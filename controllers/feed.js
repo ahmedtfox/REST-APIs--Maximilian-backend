fa420118ba60752fe6d1697a9b23ad5371883a68
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-11-05 09:30:19 +0200

    376. Adding a Create Post Endpoint

--- controllers/feed.js
+++ controllers/feed.js
@@ -15,4 +15,24 @@ exports.getPosts = (req, res, next) => {
   ];
   res.status(200).json(posts);
 };
-exports.createPost = (req, res, next) => {};
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  const postCreated = [
+    {
+      _id: new Date().toISOString(),
+      title,
+      content,
+      imageUrl: "images/77.jpg",
+      creator: {
+        name: "ahmed",
+      },
+      createdAt: new Date(),
+    },
+  ];
+
+  res.status(201).json({
+    message: "post created successfully!",
+    post: postCreated,
+  });
+};
