09e90d965946f9d428a5de5df85219a768c7eb51
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-12-04 16:10:43 +0200

    470. Organizing Multiple Tests

---
+++ test/auth-middleware.mjs
@@ -0,0 +1,28 @@
+import { expect } from "chai";
+import isAuth from "../middlewares/is-auth.js";
+
+describe("Auth Middleware", () => {
+  it("should throw an error if no Authorization header is present", () => {
+    const req = {
+      headers: {
+        Authorization: null,
+      },
+    }; // Simulating no Authorization header
+    const res = {};
+    const next = () => {};
+
+    expect(() => isAuth(req, res, next)).to.throw("Invalid token");
+  });
+
+  it("should throw an error if the authorization header is only one string", function () {
+    const req = {
+      headers: {
+        Authorization: "xyz",
+      },
+    }; // Simulating no Authorization header
+    const res = {};
+    const next = () => {};
+
+    expect(() => isAuth(req, res, next)).to.throw();
+  });
+});
