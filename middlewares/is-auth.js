380ecb43fada69726f11fd6d99428ebac94b7b3d
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-12-04 16:27:32 +0200

    471. What Not To Test!

--- middlewares/is-auth.js
+++ middlewares/is-auth.js
@@ -17,22 +17,22 @@ module.exports = (req, res, next) => {
     throw err;
   }
 
-  try {
-    const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-    const result = jwt.verify(token[1], JWT_SECRET_KEY);
-    if (result) {
-      req.userId = result.userId; // Set user info to request object
-      return next();
-    } else {
-      const err = new Error("Invalid token");
-      err.statusCode = 401;
-      throw err;
-    }
-  } catch (err) {
+  // try {
+  const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+  const result = jwt.verify(token[1], JWT_SECRET_KEY);
+  if (result) {
+    req.userId = result.userId; // Set user info to request object
+    return next();
+  } else {
+    const err = new Error("Invalid token");
+    err.statusCode = 401;
+    throw err;
+  }
+  /* } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
     next(err);
     return next(err);
-  }
+  } */
 };
