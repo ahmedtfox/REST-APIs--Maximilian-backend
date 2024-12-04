c2dec546f2dc39b12362b1b9d24a30b0becabbd0
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-12-04 16:12:00 +0200

    modify is-auth.js to suit the previous test cases

--- middlewares/is-auth.js
+++ middlewares/is-auth.js
@@ -1,18 +1,25 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = async (req, res, next) => {
-  try {
-    const tokenHeaders =
-      req.headers["Authorization"] || req.headers["authorization"];
-    if (!tokenHeaders) {
+module.exports = (req, res, next) => {
+  let token;
+  const tokenHeaders =
+    req.headers["Authorization"] || req.headers["authorization"];
+  if (tokenHeaders) {
+    token = tokenHeaders.split(" ");
+    if (!token[1]) {
       const err = new Error("Invalid token");
       err.statusCode = 401;
       throw err;
     }
+  } else {
+    const err = new Error("Invalid token");
+    err.statusCode = 401;
+    throw err;
+  }
 
-    const token = tokenHeaders.split(" ")[1];
+  try {
     const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-    const result = jwt.verify(token, JWT_SECRET_KEY);
+    const result = jwt.verify(token[1], JWT_SECRET_KEY);
     if (result) {
       req.userId = result.userId; // Set user info to request object
       return next();
