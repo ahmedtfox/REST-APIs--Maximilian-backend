ff15b80a069ebdaba8c69eb6ca4d7f8597425e93
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-12-05 16:02:29 +0200

    476. Testing Code With An Active Database

--- test/auth-conroller.mjs
+++ test/auth-conroller.mjs
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import Sinon, { stub } from "sinon";
 import auth from "../controllers/auth.js";
 import User from "../model/user.js";
-
+import dbConnect from "../utils/dbConnect.js";
 describe("Auth Middleware", () => {
   it("should throw a 500 error if accessing the database fails", async function (done) {
     // Stub the findOne method to throw an error
@@ -26,5 +26,40 @@ describe("Auth Middleware", () => {
       }
     };
     auth.login(req, res, next);
+  });
+  it("should send a response with a valid user status for an existing user", async function () {
+    try {
+      await dbConnect("test");
+      const newUser = new User({
+        email: "test@test.com",
+        password: "tester",
+        name: "test",
+        posts: [],
+      });
+      const user = await newUser.save();
+      const req = {
+        userId: user.id,
+      };
+      const res = {
+        statusCode: 500,
+        userStatus: null,
+        status: function (code) {
+          this.statusCode = code;
+          return this;
+        },
+        json: function (data) {
+          this.userStatus = data.status;
+        },
+      };
+      const next = () => {};
+      await auth.getStatus(req, res, next).then(() => {
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.userStatus).to.be.equal("i am new");
+        //done(); // Signal Mocha that the test is done
+      });
+      //done();
+    } catch (error) {
+      console.log(error); // Pass assertion errors to Mocha
+    }
   });
 });
