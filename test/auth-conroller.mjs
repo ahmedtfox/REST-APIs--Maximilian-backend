ab2870dadf97d2e2866cdc531d6cc6b68bfbb02c
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-12-05 19:47:17 +0200

    478. Hooks and remove try catch

--- test/auth-conroller.mjs
+++ test/auth-conroller.mjs
@@ -4,6 +4,16 @@ import auth from "../controllers/auth.js";
 import User from "../model/user.js";
 import { dbConnect, dbDisconnect } from "../utils/dbConnect.js";
 describe("Auth Controller", () => {
+  before(async function () {
+    try {
+      await dbConnect("test");
+      await User.deleteMany({});
+    } catch (err) {}
+  });
+  after(async function () {
+    await User.deleteMany({});
+    await dbDisconnect();
+  });
   it("should throw a 500 error if accessing the database fails", async function (done) {
     // Stub the findOne method to throw an error
     Sinon.stub(User, "findOne").throws();
@@ -27,42 +37,36 @@ describe("Auth Controller", () => {
     };
     auth.login(req, res, next);
   });
+
   it("should send a response with a valid user status for an existing user", async function () {
     const dummyUserId = "6751afcb1e336b983c0d630a";
-    try {
-      await dbConnect("test");
-      const newUser = new User({
-        email: "test@test.com",
-        password: "tester",
-        name: "test",
-        posts: [],
-        _id: dummyUserId,
-      });
-      await newUser.save();
-      const req = {
-        userId: dummyUserId,
-      };
-      const res = {
-        statusCode: 500,
-        userStatus: null,
-        status: function (code) {
-          this.statusCode = code;
-          return this;
-        },
-        json: function (data) {
-          this.userStatus = data.status;
-        },
-      };
-      const next = () => {};
-      await auth.getStatus(req, res, next).then(() => {
-        expect(res.statusCode).to.be.equal(200);
-        expect(res.userStatus).to.be.equal("i am new");
-      });
-    } catch (error) {
-      throw error;
-    } finally {
-      await User.deleteMany({});
-      await dbDisconnect();
-    }
+
+    const newUser = new User({
+      email: "test@test.com",
+      password: "tester",
+      name: "test",
+      posts: [],
+      _id: dummyUserId,
+    });
+    await newUser.save();
+    const req = {
+      userId: dummyUserId,
+    };
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status: function (code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function (data) {
+        this.userStatus = data.status;
+      },
+    };
+    const next = () => {};
+    await auth.getStatus(req, res, next).then(() => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(res.userStatus).to.be.equal("i am new");
+    });
   });
 });
